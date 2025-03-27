1476deff63cc61abe14e25519047775285f279de
Bhanuprakash0819 <chokkallabhanu@gmail.com>
2025-03-27 17:43:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Categories from './Components/Categories';
 import Dashboard from './Components/Dashboard';
 import Nav from './Components/Nav';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Nav/>
       <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/categories" element={<Categories />} />
       </Routes>
