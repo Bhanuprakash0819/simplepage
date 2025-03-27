af7fd9c9bf0c6e378e3c17467cfbe730f7b426b9
Bhanuprakash0819 <chokkallabhanu@gmail.com>
2025-03-27 17:33:48 +0530

    Initial commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "./Nav.scss";
 import { Link } from 'react-router-dom';
+
 const App = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +11,9 @@ const App = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  // Function to close menu when a link is clicked
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <div className='app-container'>
@@ -21,12 +25,12 @@ const App = () => {
           </button>
         )}
         <nav className={`nav-links ${isMobile && !isOpen ? 'hidden' : ''}`}>
-          <Link to='/dashboard' className='dash'>Dashboard</Link>
-          <Link to='/categories' className='dash'>Categories</Link>
-          <Link to='/dashboard' className='dash'>Inventory</Link>
-          <Link to='/dashboard' className='dash'>Orders</Link>
-          <Link to='/dashboard' className='dash'>Payments</Link>
-          <Link to='/dashboard' className='dash'>Support</Link>
+          <Link to='/dashboard' className='dash' onClick={closeMenu}>Dashboard</Link>
+          <Link to='/categories' className='dash' onClick={closeMenu}>Categories</Link>
+          <Link to='/inventory' className='dash' onClick={closeMenu}>Inventory</Link>
+          <Link to='/orders' className='dash' onClick={closeMenu}>Orders</Link>
+          <Link to='/payments' className='dash' onClick={closeMenu}>Payments</Link>
+          <Link to='/support' className='dash' onClick={closeMenu}>Support</Link>
         </nav>
       </div>
     </div>
