0fe59a70e92883832bb86acd32d60095fbc39ae5
Bhanuprakash0819 <chokkallabhanu@gmail.com>
2025-03-27 17:11:56 +0530

    Initial commit

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -4,18 +4,21 @@ import tshirt from './Assets/red tshirt.jpg'
 const Categories = () => {
   return (
     <div className='categories'>
-      <div className='list'>
+        
+        <div className='list'>
       <img src={tshirt} alt="Description of image"/>
        <h3>Mens</h3>
       </div>
       <div className='list'>
       <img src={tshirt} alt="Description of image"/>
-      <h3>women</h3>
+      <h3>Women</h3>
       </div>
       <div className='list'>
         <img src={tshirt} alt="Description of image"/>
-      <h3>kids</h3>
+      <h3>Kids</h3>
       </div>
+        
+     
     </div>
   )
 }
